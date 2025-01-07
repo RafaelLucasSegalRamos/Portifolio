ac17e04e49b2a575433c9e96289a5c6fcba897f1
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2025-01-07 19:01:09 -0300

    pagina de adicionar usuários

--- API_Rest/pythonProject/API_test.py
+++ API_Rest/pythonProject/API_test.py
@@ -5,4 +5,4 @@ url = "http://127.0.0.1:5000/cadastro"
 result = requests.get(url)
 
 for k, i in result.json().items():
-    print(f'ID: {k} | Nome: {i["nome"]} | Email: {i["email"]} | Senha: {i["senha"]}')
+    print(f'ID: {k} | Nome: {i["nome"]} | Email: {i["email"]} | Senha: {i["senha"]} | Idade: {i["idade"]}')
--- API_Rest/pythonProject/main.py
+++ API_Rest/pythonProject/main.py
@@ -3,13 +3,13 @@ from flask import Flask, jsonify, request, render_template
 from flask_pydantic_spec import FlaskPydanticSpec, Request, Response
 from pydantic import BaseModel, Field
 from typing import List, Optional
-import mysql.connector # Biblioteca utilizada: mysql-connector-python
+# Biblioteca utilizada: mysql-connector-python
 from time import sleep
 
 connection = connect(host="localhost", user="root", password="1234", database="bd_teste")
 cursor = connection.cursor()
 cursor.execute(
-    "create table if not exists usuarios (idUser int auto_increment primary key, Nome_usu varchar(50), email varchar(50), senha varchar(50))")
+    "create table if not exists usuarios (idUser int auto_increment primary key, Nome_usu varchar(50), email varchar(50), senha varchar(50), idade int default 0)")
 app = Flask(__name__)
 
 spec = FlaskPydanticSpec("flask", title="Teste online")
@@ -20,11 +20,37 @@ class Pessoa(BaseModel):
     nome: str
     email: str
     senha: str
+    idade: int
 
 
 @app.route('/')
 def homepage():
     return render_template('index.html', Marca="Rafael")
+
+@app.route('/cadastrar')
+def Cadastra_page():
+    return render_template('cadastrar.html', Marca="Rafael")
+
+@app.route('/cadastrar/processar', methods=['POST'])
+def processar():
+    try:
+        # Adicione depuração para verificar os dados recebidos
+        # print(request.form)
+        nome_usu = request.form.get('nome')
+        email_usu = request.form.get('email')
+        senha_usu = request.form.get('senha')
+        idade = request.form.get('idade')
+
+        if not nome_usu or not email_usu or not senha_usu or not idade:
+            return jsonify({'error': 'Dados incompletos'}), 400
+        nome_usu = nome_usu.strip().title().replace(' ', '_').replace("'", "").replace(")", "").replace("(", "").replace(",", "").replace(".", "").replace("!", "").replace("?", "").replace(":", "").replace(";", "").replace("/", "").replace("=", "").replace("+", "").replace("-", "").replace("*", "").replace("&", "").replace("%", "").replace("$", "").replace("#", "").replace("@", "").replace("--", "")
+        email_usu = email_usu.strip().replace(' ', '_').replace("'", "").replace(")", "").replace("(", "").replace(",", "").replace(".", "").replace("!", "").replace("?", "").replace(":", "").replace(";", "").replace("/", "").replace("=", "").replace("+", "").replace("-", "").replace("*", "").replace("&", "").replace("%", "").replace("$", "").replace("#", "").replace("--", "")
+        senha_usu = senha_usu.strip().replace("'", "").replace(")", "").replace("(", "").replace(",", "").replace(".", "").replace(":", "").replace(";", "").replace("/", "").replace("=", "").replace("+", "").replace("-", "").replace("*", "").replace("&", "").replace("%", "").replace("$", "").replace("#", "").replace("--", "")
+        cursor.execute(f"insert into usuarios (Nome_usu, email, senha, idade) values ('{nome_usu}', '{email_usu}', '{senha_usu}', '{request.form.get('idade')}')")
+        connection.commit()
+        return jsonify({'message': f'Nome: {nome_usu}, Email: {email_usu}, Senha: {senha_usu}, Idade: {request.form.get('idade')}'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 
 @app.get('/cadastro')  # Puxando os dados do banco de dados e colocando na API
@@ -34,7 +60,7 @@ def cadastro():
     cursor.execute('select * from usuarios')
     resultado = cursor.fetchall()
     for linha in resultado:
-        resultado_format[linha[0]] = {"nome": linha[1], "email": linha[2], "senha": linha[3]}
+        resultado_format[linha[0]] = {"nome": linha[1], "email": linha[2], "senha": linha[3], "idade": linha[4]}
 
     return jsonify(resultado_format)
 
@@ -63,7 +89,7 @@ def post_usu():
         ".", "").replace(":", "").replace(";", "").replace("/", "").replace("=", "").replace("+", "").replace("-",
                                                                                                               "").replace(
         "*", "").replace("&", "").replace("%", "").replace("$", "").replace("#", "").replace("--", "")
-    cursor.execute(f"insert into usuarios (Nome_usu, email, senha) values ('{nome_usu}', '{email_usu}', '{senha_usu}')")
+    cursor.execute(f"insert into usuarios (Nome_usu, email, senha) values ('{nome_usu}', '{email_usu}', '{senha_usu}', '{body['idade']}')")
     connection.commit()
     return jsonify(body)
 
@@ -120,4 +146,5 @@ def deleta_id(id: int):
     return f"Usuário com os dados: ID: {resultado[0]}, Nome: {resultado[1]} e Email: {resultado[2]} foi deletado com sucesso!"
 
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
