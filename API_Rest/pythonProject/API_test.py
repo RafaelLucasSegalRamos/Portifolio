c85272168957e35b32f728a32151d45d8b8fe8b5
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2025-01-06 23:03:59 -0300

    now_working_API_Rest

---
+++ API_Rest/pythonProject/API_test.py
@@ -0,0 +1,8 @@
+import requests
+
+url = "http://127.0.0.1:5000/cadastro"
+
+result = requests.get(url)
+
+for k, i in result.json().items():
+    print(f'ID: {k} | Nome: {i["nome"]} | Email: {i["email"]} | Senha: {i["senha"]}')
--- API_Rest/pythonProject/main.py
+++ API_Rest/pythonProject/main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from MySQLdb import connect
 from flask import Flask, jsonify, request, render_template
 from flask_pydantic_spec import FlaskPydanticSpec, Request, Response
 from pydantic import BaseModel, Field
 from typing import List, Optional
-import mysql.connector
+import mysql.connector # Biblioteca utilizada: mysql-connector-python
 from time import sleep
 
-connection = mysql.connector.connect(host="localhost", user="*", password="*", database="bd_teste")
+connection = connect(host="localhost", user="root", password="1234", database="bd_teste")
 cursor = connection.cursor()
 cursor.execute(
     "create table if not exists usuarios (idUser int auto_increment primary key, Nome_usu varchar(50), email varchar(50), senha varchar(50))")
@@ -14,3 +14,110 @@ app = Flask(__name__)
 
 spec = FlaskPydanticSpec("flask", title="Teste online")
 spec.register(app)
+
+
+class Pessoa(BaseModel):
+    nome: str
+    email: str
+    senha: str
+
+
+@app.route('/')
+def homepage():
+    return render_template('index.html', Marca="Rafael")
+
+
+@app.get('/cadastro')  # Puxando os dados do banco de dados e colocando na API
+@spec.validate(resp=Response())
+def cadastro():
+    resultado_format = {}
+    cursor.execute('select * from usuarios')
+    resultado = cursor.fetchall()
+    for linha in resultado:
+        resultado_format[linha[0]] = {"nome": linha[1], "email": linha[2], "senha": linha[3]}
+
+    return jsonify(resultado_format)
+
+
+@app.post('/cadastro')  # Colocando novos dados no banco de dados e na API
+@spec.validate(body=Request(Pessoa), resp=Response(HTTP_200=Pessoa))
+def post_usu():
+    body = request.context.body.dict()
+    nome_usu = body["nome"].strip().title().replace(' ', '_').replace("'", "").replace(")", "").replace("(",
+                                                                                                        "").replace(",",
+                                                                                                                    "").replace(
+        ".", "").replace("!", "").replace("?", "").replace(":", "").replace(";", "").replace("/", "").replace("=",
+                                                                                                              "").replace(
+        "+", "").replace("-", "").replace("*", "").replace("&", "").replace("%", "").replace("$", "").replace("#",
+                                                                                                              "").replace(
+        "@", "").replace("--", "")
+    email_usu = body["email"].strip().replace(' ', '_').replace("'", "").replace(")", "").replace("(",
+                                                                                                  "").replace(
+        ",", "").replace(".", "").replace("!", "").replace("?", "").replace(":", "").replace(";", "").replace("/",
+                                                                                                              "").replace(
+        "=", "").replace("+", "").replace("-", "").replace("*", "").replace("&", "").replace("%", "").replace("$",
+                                                                                                              "").replace(
+        "#", "").replace("--", "")
+    senha_usu = body["senha"].strip().replace("'", "").replace(")", "").replace("(", "").replace(",",
+                                                                                                 "").replace(
+        ".", "").replace(":", "").replace(";", "").replace("/", "").replace("=", "").replace("+", "").replace("-",
+                                                                                                              "").replace(
+        "*", "").replace("&", "").replace("%", "").replace("$", "").replace("#", "").replace("--", "")
+    cursor.execute(f"insert into usuarios (Nome_usu, email, senha) values ('{nome_usu}', '{email_usu}', '{senha_usu}')")
+    connection.commit()
+    return jsonify(body)
+
+
+@app.put('/cadastro/<int:id>')  # Substituindo dados no banco de dados e na API
+@spec.validate(body=Request(Pessoa), resp=Response())
+def altera_usuario(id: int):
+    body = request.context.body.dict()
+    nome_usu = body["nome"].strip().title().replace(' ', '_').replace("'", "").replace(")", "").replace("(",
+                                                                                                        "").replace(
+        ",", "").replace(".", "").replace("!", "").replace("?", "").replace(":", "").replace(";", "").replace("/",
+                                                                                                              "").replace(
+        "=", "").replace("+", "").replace("-", "").replace("*", "").replace("&", "").replace("%", "").replace("$",
+                                                                                                              "").replace(
+        "#", "").replace("@", "")
+    email_usu = body["email"].strip().replace(' ', '_').replace("'", "").replace(")", "").replace("(",
+                                                                                                  "").replace(
+        ",", "").replace(".", "").replace("!", "").replace("?", "").replace(":", "").replace(";", "").replace("/",
+                                                                                                              "").replace(
+        "=", "").replace("+", "").replace("-", "").replace("*", "").replace("&", "").replace("%", "").replace("$",
+                                                                                                              "").replace(
+        "#", "").replace("--", "")
+    senha_usu = body["senha"].strip().replace("'", "").replace(")", "").replace("(", "").replace(",",
+                                                                                                 "").replace(
+        ".", "").replace(":", "").replace(";", "").replace("/", "").replace("=", "").replace("+", "").replace("-",
+                                                                                                              "").replace(
+        "*", "").replace("&", "").replace("%", "").replace("$", "").replace("#", "").replace("--", "")
+    cursor.execute(
+        f"update usuarios set Nome_usu = '{nome_usu}', email = '{email_usu}', senha = '{senha_usu}' where idUser = {id}")
+    connection.commit()
+    return jsonify(body)
+
+
+@app.get('/cadastro/<int:id>')  # Procurando dados no banco de dados e na API via ID
+@spec.validate(resp=Response())
+def procura_id(id: int):
+    resultado_format = {}
+    cursor.execute(f'select * from usuarios where idUser = {id}')
+    resultado = cursor.fetchall()
+    for linha in resultado:
+        resultado_format[linha[0]] = {"nome": linha[1], "email": linha[2], "senha": linha[3]}
+
+    return jsonify(resultado_format)
+
+
+@app.delete('/cadastro/<int:id>')  # Deletando dados no banco de dados e na API via ID
+@spec.validate(resp=Response())
+def deleta_id(id: int):
+
+    cursor.execute(f'select * from usuarios where idUser = {id}')
+    resultado = cursor.fetchall()[0]
+    cursor.execute(f'delete from usuarios where idUser = {id}')
+    connection.commit()
+    return f"Usuário com os dados: ID: {resultado[0]}, Nome: {resultado[1]} e Email: {resultado[2]} foi deletado com sucesso!"
+
+
+app.run()
